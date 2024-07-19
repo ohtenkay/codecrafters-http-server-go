94a262063afa732118d5b76a5c2e039ab488d37d
Ondřej Hložek <hlozek.o1@gmail.com>
2024-07-19 13:20:18 +0200

    respond with body

--- app/server.go
+++ app/server.go
@@ -8,10 +8,8 @@ import (
 )
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
@@ -28,9 +26,17 @@ func main() {
 	conn.Read(b)
 	parts := bytes.Split(b, []byte(" "))
 
-	if !(string(parts[1]) == "/") {
-		conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	if string(parts[1]) == "/" {
+		conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 	}
 
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	urlParts := bytes.Split(parts[1], []byte("/"))
+	if string(urlParts[1]) == "echo" {
+		length := len(urlParts[2])
+
+		conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", length) + "\r\n\r\n"))
+		conn.Write(urlParts[2])
+	}
+
+	conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
 }
